3d4514889f66428f1d0c8df642328bb099b10e88
Pankaj Kumar Bijarniya <pankajbijarniya2004@gmail.com>
2023-07-10 21:35:44 +0530

    updated redux

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
+"use client";
 import Intro from "@/components/landingPage/intro";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const themeMode = useSelector((state) => state.theme.mode);
+
   return (
     <div>
       <Intro />
